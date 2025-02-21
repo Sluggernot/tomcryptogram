91906b11f6f8b2f29de98ce8ccd78146131924c0
Slugger not <ben.miles82@gmail.com>
2025-02-21 18:16:08 -0500

    Move to a functioning class for magic squares
    Started with jsut a main.cpp. Needed to convert to a class, at this time. Now can use MagicSquare_data objects. Crappy name.
    Also, all funcs that can be const and all that are in place.

--- MagicSquares/MagicSquare_data.cpp
+++ MagicSquares/MagicSquare_data.cpp
@@ -3,3 +3,169 @@
 //
 
 #include "MagicSquare_data.h"
+
+#include <set>
+
+void MagicSquare_data::printMagicSquare() const {
+    for (int i = 0; i < 9; i++) {
+        std::cout << square_arr[i].second << " ";
+        if (i != 0 && ((i+1) % 3) == 0) { std::cout << std::endl;}
+    }
+}
+
+void MagicSquare_data::printMagicSquare_withSums(const bool diags) const {
+    mpz_int sum = 0;
+    for (int i = 0; i < 9; i++) {
+        std::cout << square_arr[i].second << "\t ";
+        sum += square_arr[i].second;
+        if (i != 0 && ((i+1) % 3) == 0) { std::cout <<"\t - " << sum << std::endl; sum = 0;}
+    }
+    std::cout << "==================================================" << std::endl;
+    for (int i = 0; i < 3; ++i) {
+        std::cout << square_arr[i].second + square_arr[i+3].second + square_arr[i+6].second << "\t ";
+    }
+    if (diags) {
+        std::cout << "\nDiagonals:\nUpper Left to Low Right: ";
+        std::cout << square_arr[0].second + square_arr[4].second + square_arr[8].second << std::endl;
+        std::cout << "Bottom Left to Up Right: ";
+        std::cout << square_arr[2].second + square_arr[4].second + square_arr[6].second << std::endl;
+    }
+}
+
+char MagicSquare_data::printLocation(const int index) const {
+    if (index == 0 || index == 3 || index == 6) {
+        std::cout << "Left ";
+    }
+    else if (index == 1 || index == 4 || index == 7) {
+        std::cout << "Center ";
+    }
+    else if (index == 2 || index == 5 || index == 8) {
+        std::cout << "Right ";
+    }
+
+    if (index == 0 || index == 1 || index == 2) {
+        std::cout << "Top\n";
+    }
+    else if (index == 3 || index == 4 || index == 5) {
+        std::cout << "Center\n";
+    }
+    else if (index == 6 || index == 7 || index == 8) {
+        std::cout << "Bottom\n";
+    }
+    return ' ';
+}
+
+void MagicSquare_data::printMagicSquareDetails() const {
+    int number = 0;
+    for (int i = 1; i < 9; i++) {
+        if (square_arr[i].second < square_arr[number].second) {
+            number = i;
+        }
+    }
+    std::cout << "Smallest: " << printLocation(number) << " Val: " << square_arr[number].second << std::endl;
+    number = 0;
+    for (int i = 1; i < 9; i++) {
+        if (square_arr[i].second > square_arr[number].second) {
+            number = i;
+        }
+    }
+    std::cout << "Largest:" << printLocation(number) << " Val: " << square_arr[number].second << std::endl;
+
+    mpz_int sum = 0;
+    for (const auto & i : square_arr) {
+        sum += i.second;
+    }
+    std::cout << "Sum: " << sum << std::endl;
+
+    std::cout << "\n\nAscending list: " << std::endl;
+    std::set<int> numbers;
+    std::vector<int> the_numbers;
+    int firstLowest = getLowestValuesIndex();
+    numbers.insert(firstLowest);
+    the_numbers.push_back(firstLowest);
+    bool found = false;
+    while (the_numbers.size() < 9) {
+        for (int i = 0; i < 9; i++) {
+            if (!found && !numbers.contains(i) && square_arr[i] > square_arr[firstLowest]) {
+                found = true;
+                firstLowest = i;
+            }
+            else if (found && !numbers.contains(i) && square_arr[i] < square_arr[firstLowest]) {
+                firstLowest = i;
+            }
+        }
+        the_numbers.push_back(firstLowest);
+        numbers.insert(firstLowest);
+        found = false;
+    }
+    for (const int the_number : the_numbers) {
+        std::cout << "Sqrt: " << square_arr[the_number].first << " Value: " << square_arr[the_number].second << std::endl;
+    }
+}
+
+bool MagicSquare_data::isMagicSquare() const {
+
+    //Are all numbers unique?
+    for (int i = 0; i < 9; i++) {
+        for (int j = i+1; j < 9; j++) {
+            //if two values are the same, it's not a magic square
+            if (square_arr[i].second != square_arr[j].second) { return false; }
+        }
+    }
+
+    //Add and check cols, rows, diags.
+    const mpz_int row1 = square_arr[0].second + square_arr[1].second + square_arr[2].second;
+    const mpz_int row2 = square_arr[3].second + square_arr[4].second + square_arr[5].second;
+    if (row1 != row2) { return false; }
+    const mpz_int row3 = square_arr[6].second + square_arr[7].second + square_arr[8].second;
+    if (row2 != row3) { return false; }
+
+    const mpz_int col1 = square_arr[0].second + square_arr[3].second + square_arr[6].second;
+    if (col1 != row1) { return false; }
+    const mpz_int col2 = square_arr[1].second + square_arr[4].second + square_arr[7].second;
+    if (col1 != col2) { return false; }
+    const mpz_int col3 = square_arr[2].second + square_arr[5].second + square_arr[8].second;
+    if (col2 != col3) { return false; }
+
+    //Diagonals
+    const mpz_int dia1 = square_arr[0].second + square_arr[4].second + square_arr[8].second;
+    if (dia1 != col3) { return false; }
+    const mpz_int dia2 = square_arr[2].second + square_arr[4].second + square_arr[6].second;
+    if (dia1 != dia2) { return false; }
+
+    std::cout << "Perfect square found!" << std::endl;
+    printMagicSquare();
+    return true;
+}
+
+void MagicSquare_data::swapLowest(const int counter) {
+    int lowest = getLowestValuesIndex();
+    square_arr[lowest].second = counter * counter;
+}
+
+//Get the index into the array of the lowest value. Can use first or second.
+int MagicSquare_data::getLowestValuesIndex() const {
+    int lowest = 0;
+    for (int i = 1; i < 9; i++) {
+        if (square_arr[i].second < square_arr[lowest].second) {
+            lowest = i;
+        }
+    }
+    return lowest;
+}
+
+void MagicSquare_data::incrementAValueAtIndex(const int index) {
+    ++square_arr[index].first;
+    square_arr[index].second = square_arr[index].first * square_arr[index].first;
+}
+
+void MagicSquare_data::decrementAValueAtIndex(const int index) {
+    --square_arr[index].first;
+    square_arr[index].second = square_arr[index].first * square_arr[index].first;
+}
+
+void MagicSquare_data::initializeSquares(std::pair<mpz_int, mpz_int>* square_arr) {
+    for (int i = 0; i < 9; ++i) {
+        square_arr[i].first = sqrt(square_arr[i].second);
+    }
+}
--- MagicSquares/MagicSquare_data.h
+++ MagicSquares/MagicSquare_data.h
@@ -5,9 +5,50 @@
 #ifndef MAGICSQUARE_DATA_H
 #define MAGICSQUARE_DATA_H
 
+#include <boost/multiprecision/gmp.hpp>
 
+using namespace boost::multiprecision;
 
 class MagicSquare_data {
+private:
+
+    std::pair<mpz_int, mpz_int> square_arr[9];
+
+    //Helper func to get us so initialization
+    void initializeSquares(std::pair<mpz_int, mpz_int>* square_arr);
+
+public:
+
+    MagicSquare_data() {
+        //These magic numbers are just a starting point from: https://www.youtube.com/watch?v=2Twa-z_WPE4
+        square_arr[0] = std::make_pair(1, 5976563248318113025ULL);
+        square_arr[1] = std::make_pair(1, 15260232026299743025ULL);
+        square_arr[2] = std::make_pair(1, 2577330938902409089ULL);
+        square_arr[3] = std::make_pair(1, 4537991882387127025ULL);
+        square_arr[4] = std::make_pair(1, 7937944177203705025ULL);
+        square_arr[5] = std::make_pair(1, 11337896472020283025ULL);
+        square_arr[6] = std::make_pair(1, 13298557415505000961ULL);
+        square_arr[7] = std::make_pair(1, 615656328107667025ULL);
+        square_arr[8] = std::make_pair(1, 9899325106089297025ULL);
+        initializeSquares(square_arr);
+    }
+
+    void printMagicSquare() const;
+    void printMagicSquare_withSums(const bool diags = true) const;
+    void printMagicSquareDetails() const;
+    char printLocation(const int index) const;
+    bool isMagicSquare() const;
+
+    //Probably not useful to arbitrarily pick the lowest value.
+    void swapLowest(const int counter);
+    //Just finds the lowest of the 9 values and returns the index
+    int getLowestValuesIndex() const;
+
+    //Takes an index and increments or decrements by 1, by raising or lowering .first and then squaring it
+    //Saves us from using sqrt, ever so far.
+    void incrementAValueAtIndex(const int index);
+    void decrementAValueAtIndex(const int index);
+
 
 };
 
