2ccac7fd0bae3e60d89787cc49669600e90f7123
Slugger not <ben.miles82@gmail.com>
2025-03-22 07:57:55 -0400

    Just messing aruond with power of five shit
    its works and is fairly efficient

--- PowersOfFive/main.cpp
+++ PowersOfFive/main.cpp
@@ -3,40 +3,41 @@
 //
 #include <iostream>
 #include <boost/multiprecision/gmp.hpp>
-#include <set>
 using namespace boost::multiprecision;
 
 mpz_int toPowerOfFive(const mpz_int& incoming);
+mpz_int toPowerOfN(const mpz_int& incoming, const unsigned int N);
+
+bool findAnAlternativeWay(const mpz_int& target, const mpz_int& a, const mpz_int& b);
 
 int main (int argc, const char * argv[]) {
 
-  constexpr int initialLimit = 10000;
+  constexpr int initialLimit = 100;
 
-  mpz_int start = 1;
-  mpz_int limit = initialLimit;
+  mpz_int start = 48800;
+  mpz_int limit = start + initialLimit;
 
-  std::set<mpz_int> totalsWeveSeen;
+  //Do we try to calculate
+  //Something like start with 1000^5.
+  std::cout << "Starting with : " << start << std::endl;
+
   mpz_int total = 0;
+  mpz_int mpz_i;//, mpz_j;
   while (true) {
     for (mpz_int i = start; i <= limit; ++i) {
+      mpz_i = toPowerOfFive(i);
       for (mpz_int j = i+1; j <= limit; ++j) {
-        total = toPowerOfFive(i) + toPowerOfFive(j);
-        if (!totalsWeveSeen.contains(total)) {
-          totalsWeveSeen.insert(total);
-        }
-        else {
+        total = mpz_i+toPowerOfFive(j);
+        if (findAnAlternativeWay(total, i, j)) {
           std::cout << "Holy shit we've seen: " << total << " We're on: " << i << ' ' << j << std::endl;
-          std::cout << "We checked: " << totalsWeveSeen.size() << " values" << std::endl;
           return 0;
         }
       }
     }
-    std::cout <<"Hit limit: " << limit << std::endl;
+    std::cout <<"Hit limit: " << limit << "  Total so far: " << total << "\n";
     start = limit;
     limit += initialLimit;
   }
-
-  std::cout << "We checked: " << totalsWeveSeen.size() << " values" << std::endl;
 
   return 0;
 }
@@ -45,3 +46,40 @@ mpz_int toPowerOfFive(const mpz_int& incoming) {
   return incoming * incoming * incoming * incoming * incoming;
 }
 
+mpz_int toPowerOfN(const mpz_int& incoming, const unsigned int N) {
+  mpz_int total = incoming;
+  for (unsigned i = 1; i <= N; ++i) {
+    total *= incoming;
+  }
+  return total;
+}
+
+bool searchMeeting(const mpz_int& target, const mpz_int& a, const mpz_int& b) {
+  mpz_int aVal = 0;
+  for (mpz_int i = a-1; i >= b; --i) {
+    if (i == b) continue;
+    aVal = toPowerOfFive(i);
+    for (mpz_int j = b+1; j <= i; ++j) {
+      if (aVal + toPowerOfFive(j) == target) { std::cout << "FOUND A HIT!: " << i << " - " << j << std::endl; return true; }
+    }
+  }
+  return false;
+}
+
+bool findAnAlternativeWay(const mpz_int& target, const mpz_int& a, const mpz_int& b) {
+
+  mpz_int aVal = 0;
+  mpz_int totalCheck = 0;
+  for (mpz_int i = a+1; ; ++i) {
+    if (i == b) continue;
+    aVal = toPowerOfFive(i);
+    if (aVal > 2 * target) break;
+    for (mpz_int j = b-(i-a); j >= 0; --j) {
+      totalCheck = aVal + toPowerOfFive(j);
+      if (totalCheck > target) break;
+      if (totalCheck == target) { std::cout << "FOUND A HIT!: " << i << " - " << j << std::endl; return true; }
+    }
+  }
+
+  return searchMeeting(target, a, b);
+}
